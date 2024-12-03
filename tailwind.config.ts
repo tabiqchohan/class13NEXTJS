79e111c26a8027e1b3155b7f70a8feb7b7ca4adf
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-12-03 23:11:13 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import { plugin } from "postcss";
+import type { Config } from "tailwindcss";
+
+export default {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugin:{},
+   
+}
